79a70ff4e9fc8e613b171921fc529a88da6931ee
uttam pun <uttampun50@gmail.com>
2025-02-08 21:01:00 +0545

    feat(form-and-query): react hook form and react query implements

---
+++ app/signup/signup.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const signup = () => {
+    return(
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+export default signup
