afd4b194a04d8480e91334b484cc669c6e4d0964
uttam pun <uttampun50@gmail.com>
2025-03-22 21:18:51 +0545

    chore: added Home screens after login

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -3,11 +3,13 @@ import Message from "./Message"
 import Calls from "./Calls"
 import Contact from "./contact"
 import Settings from "./settings"
+import Home from "./home"
 
 export default function TabLayout() {
     const Tabs = createBottomTabNavigator()
     return (
         <Tabs.Navigator >
+            <Tabs.Screen name="home" component={Home} options={{ headerShown: false }} />
             <Tabs.Screen name="Message" component={Message} options={{ headerShown: false }} />
             <Tabs.Screen name="Calls" component={Calls} options={{ headerShown: false }} />
             <Tabs.Screen name="Contact" component={Contact} options={{ headerShown: false }} />
