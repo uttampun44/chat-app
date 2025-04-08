a6d84dcf52899a82d2f96e5ef87c7daba478a7d7
uttampun <uttampun62@gmail.com>
2025-04-08 13:53:25 +0545

    feat: pusher packager added

--- app/screens/message/message.tsx
+++ app/screens/message/message.tsx
@@ -1,11 +1,40 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Pusher, PusherEvent } from '@pusher/pusher-websocket-react-native';
+import { useEffect } from "react";
+
 
 export default function Message() {
-    return (
-        <SafeAreaView>
-           
-            <Text>Message</Text>
-        </SafeAreaView>
-    )
+
+  const pusher = Pusher.getInstance();
+
+  const pusherConnection = async () => {
+    try {
+      await pusher.init({
+        apiKey: "d5efa0dbabfcefc2d866",
+        cluster: "ap2"
+      });
+
+      await pusher.connect();
+      await pusher.subscribe({
+        channelName: "chat",
+        onEvent: (event: PusherEvent) => {
+          console.log(`Event received: ${event}`);
+        }
+      });
+    } catch (error) {
+
+    }
+  }
+  useEffect(() => {
+    pusherConnection()
+  }, [])
+
+
+  return (
+    <SafeAreaView>
+
+      <Text>Message</Text>
+    </SafeAreaView>
+  )
 }
