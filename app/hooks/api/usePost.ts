c7bbf86c13ef82854659ab1dc14be17bb5fd28c0
uttam pun <uttampun50@gmail.com>
2025-04-13 10:07:41 +0545

    feat: header set token

--- app/hooks/api/usePost.ts
+++ app/hooks/api/usePost.ts
@@ -1,11 +1,22 @@
 import axiosInstance from "@/lib/axiosInstance";
 import { useMutation } from "@tanstack/react-query";
 
-export default function usePost<T = any>(url: string, config?: Record<string, string>) {
+interface RequestConfig {
+    headers?: Record<string, string>;
+    [key: string]: any;
+}
+
+export default function usePost<T = any>(url: string,) {
     
-    const mutation = useMutation<T, unknown, any>({
-        mutationFn: async(data) => {
-            const response = await axiosInstance.post(url, data, config);
+    const mutation = useMutation<T, unknown, RequestConfig>({
+        mutationFn: async({headers, ...data}) => {
+            const response = await axiosInstance.post(url, data, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    ...headers
+                }
+            });
             return response.data;
         },   
     })
