3bbfeb3e7ff1a6e75213fcdcd4a37bebb783445b
uttampun <uttampun62@gmail.com>
2025-04-13 14:24:04 +0545

    chore: fix conflict

--- app/index.tsx
+++ app/index.tsx
@@ -7,13 +7,8 @@ import { Stack } from "expo-router";
 export default function Index() {
   return (
      <SafeAreaView className="flex-1">
-<<<<<<< Updated upstream
         <Stack.Screen  options={{ headerShown: false, animation: 'fade' }} />
         <Splash />
-=======
-        <Stack.Screen name="splash" options={{ headerShown: false, animation: 'fade' }} component={Splash} />
-         <Splash /> 
->>>>>>> Stashed changes
       </SafeAreaView>
   );
 }
